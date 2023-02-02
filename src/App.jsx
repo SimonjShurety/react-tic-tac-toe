236359aafc24d52e0881f248b4b06a096b51234a
SimonjShurety <simonjshurety@gmail.com>
2023-02-02 10:53:16 -0300

    add state and display 'x'

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useState } from "react";
 
-function Square({ value }) {
-  return <button className="square">{value}</button>;
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
